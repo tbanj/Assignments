eb40d86a30dc7e8a922d8f1a5a7b8423aa4961f1
Engr. Alabi Temitope Wahab <t.banji@rocketmail.com>
2019-02-14 16:55:41 +0100

    src/index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import StateComponent from "./components/StateComponent";
+import RenderButton from "./components/assignmentTwo/RenderButton";
+import InputBind from "./components/assignmentTwo/InputBind";
+
 
 ReactDOM.render(
 <div>
   <StateComponent/>
 </div>, document.getElementById('root'));
+
+
+ReactDOM.render(
+  <div>
+    <RenderButton/>
+  </div>, document.getElementById('renderbutton'));
+
+
+ReactDOM.render(
+  <div>
+    <InputBind/>
+  </div>, document.getElementById('inputBind'));
 
