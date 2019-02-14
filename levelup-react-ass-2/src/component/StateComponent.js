8c759b260d7c9cd2ffa3685bd330951cd0514c35
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2019-02-14 16:51:06 +0100

    commit to react ass 2

--- levelup-react-ass-2/src/component/StateComponent.js
+++ levelup-react-ass-2/src/component/StateComponent.js
@@ -2,6 +2,41 @@ import React, { Component } from 'react';
 
 class StateComponent extends Component {
     constructor(props){
-        super(props)
+        super(props);
+        this.inputChange = this.inputChange.bind(this);
+        this.buttonClick = this.buttonClick.bind(this);
+
+        this.state = {
+            clickcount : 0,
+            typeshow : '',
+        };
     }
-}
+
+    buttonClick(event){
+        this.setState({ clickcount: this.state.clickcount + 1});
+    }
+
+    inputChange(event) {
+       const change = event.target.value;
+       this.setState({ typeshow : change });
+    }
+
+    render(){
+        const thestate = this.state;
+        return(
+            <div>
+                <button onClick = {this.buttonClick}>
+                    Click The Button
+                </button>
+                <p>You Clicked this button {thestate.clickcount} Times.</p>
+
+                <input type='text' placeholder='Type A Word' onChange={ this.inputChange } ></input>
+                <p>{thestate.typeshow}</p>
+            </div>
+        )
+    }
+
+    
+};
+
+export default StateComponent;
