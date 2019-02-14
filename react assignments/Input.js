9781126b5bc580f7a1f8cd507eeb03627e6dddc6
olakunle <oraclee10gee@gmail.com>
2019-02-14 17:04:31 +0100

    made changes to my react app

---
+++ react assignments/Input.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Input extends React.Component{
+    render() {
+        return <input type = 'text' placeholder = "person's name"/>              
+    
+    }
+
+
+
+
+}
+
+export default Input;
