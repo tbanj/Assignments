9b1078b1391013ba7169daf3a6361b23baf0443e
olakunle <oraclee10gee@gmail.com>
2019-02-14 17:09:52 +0100

    the solution.js file to the daily challenge

---
+++ daily challenge/solution.js
@@ -0,0 +1,22 @@
+const library = [
+    {
+        author: 'Bill Gates',
+        title: 'the road ahead',
+        readingStatus: true,
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter issacson',
+        readingStatus: true,
+    },
+    {
+        author: 'Suzanne Collins',
+        title: 'Mockingjay: the final book of the hunger games',
+        readingStatus: false,
+    },      
+]
+
+library.forEach(function (e) {
+    console.log(e);
+})
+
