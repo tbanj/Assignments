b4db0b31313366d37dc56f4dca5772f1fb2827ea
Temi <karatemitayo@gmail.com>
2019-02-19 15:45:56 +0100

    added state component

--- note-app/src/App.js
+++ note-app/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import EditButton from './EditButtton'
 import DeleteButton from './DeleteButton'
+import InputPersonal from './InputPersonal';
+import InputOfficial from './InputOfficial';
 /**
  * Build a simple note saving application and use Redux as a state management
  *  library. The application should use any css-in-js library for styling 
@@ -14,12 +16,12 @@ class App extends Component {
   render() {
     return (
       <div class="container p-5">
-        <div class="row py-5">
+        <div class="row p-5">
           <div class="col-xs-6 col-sm-4 col-md-4 col-lg-6">
-            <div class="card text-white bg-primary mb-3">
+            <div class="card rounded text-white bg-success mb-3">
               <div class="card-body">
-                <h5 class="card-title">Personal Note</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 class="card-title text-warning">Personal Note</h5>
+                <InputPersonal />
                 <div class="d-flex">
                   <EditButton /><span class="ml-5"><DeleteButton /></span>
                 </div>
@@ -27,10 +29,10 @@ class App extends Component {
             </div>
           </div>
           <div class="col-xs-6 col-sm-4 col-md-4 col-lg-6">
-            <div class="card text-white bg-success mb-3">
+            <div class="card rounded text-white bg-success mb-3">
               <div class="card-body">
-                <h5 class="card-title">Official Note</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 class="card-title text-warning">Official Note</h5>
+                <InputOfficial />
                 <div class="d-flex ">
                   <EditButton /><span class="ml-5"><DeleteButton /></span>
                 </div>
--- note-app/src/EditButtton.js
+++ note-app/src/EditButtton.js
@@ -1,9 +1,27 @@
 import React, {Component} from 'react';
 
 class EditButton extends Component{
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    state = {
+        count: 0
+    }
+
+    handleClick(event){
+        this.setState({count:this.state.count + 1})
+    }
+
     render(){
         return(
-            <button class="border-0 bg-light"><i class="fas fa-edit"></i></button>
+            <div>
+                <button class="border-0 bg-light px-3" 
+                onClick ={this.handleClick}><i class="fas fa-pencil-alt">
+                </i></button>
+                <p>I have been clicked {this.state.count}</p>
+            </div>
         )
     }
 }
