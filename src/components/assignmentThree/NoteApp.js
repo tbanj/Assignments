2e09b8c59bfe332b99fc2dca5376e5bca7c97e57
Engr. Alabi Temitope Wahab <t.banji@rocketmail.com>
2019-02-21 06:59:36 +0100

    working perfectly

--- src/components/assignmentThree/NoteApp.js
+++ src/components/assignmentThree/NoteApp.js
@@ -1,64 +1,88 @@
 import React from 'react';
 import './note.scss'
 
+import {store, STORE_DATA, DELETE_DATA} from './store';
+
+let id =0;
+let value = "";
 class NoteApp extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        value: 'You didnt type anything yet'
-      };
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-      console.log(event.target.value);
+
+  state = {
+    datas: []
+}
+componentDidMount(){
+    store.subscribe(() => {
+      this.setState({datas: store.getState().datas})
+    })
+  }
+  handleChange(event) {
+    value = event.target.value;
+
       
-    }
-  
-    handleSubmit(event) {
-      alert('Note you inputted have been submitted ' + this.state.value);
-      event.preventDefault();
-    }
-  
-    render() {
-        
-      return (
-          // handleSubmit will extract the data from the form
+    console.log(event.target.value);
+    
+  }
+render(){
+    const  {datas} = this.state;
+    return (
+       
+        <div>
+            <div className="jumbotron text-center">
+                <h1>Note App develop with React App</h1>
+                <p>Note Saving Application</p> 
+            </div>
+
         <div className="container">
-            <div className="row">
-            <div className="col-sm-12  col-md-12 formParent">
-            <form className="forCenter" onSubmit={this.handleSubmit}>
-            <h1> Note Application</h1>
-          <label>
-           
-            <br/>
+        <div className="row">
+            <div className="col-sm-4  offset-sm-4">
+            <div className="input-group mb-5">
+                <textarea onChange={this.handleChange} type="text" className="form-control" placeholder="Input data" ></textarea>
+                <div className="input-group-append">
+                    <button className="btn btn-success" 
+                    onClick={() => {
+                      if(value.length >=1) {
+                        store.dispatch({
+                          type: STORE_DATA,
+                          payload: {
+                            id: id++,
+                            title: value,
+                          }
+                        })
+                      } else {
+                        alert("you are yet to input any value")
+                      }
+                    }}
+                    type="button">Add Note</button>
+                </div>
+                </div>
+        </div>
+        <div className='col-sm-4 offset-sm-4'>
+        
+          
+        <ul className="list-group">
+        {   
+            this.state.datas.length === 0 ? <p>Am waiting for your input</p> : 
             
-            <textarea className="textize"  defaultValue={this.state.value} onChange={this.handleChange} />
-          </label>
-          <br/>
-          {/* <p className="changeColor"> Alabi Temitope</p> */}
-          <input className="btn btn-success" type="submit" value="Submit" />
-        </form>
-            </div>
-            </div>
-            
-
+            datas.map((data, index) => {
+              return <div key={index}>
+                 <li className="list-group-item">{data.title}
+                    <button className='btn btn-danger float-right btn-sm'
+                        onClick={() => store.dispatch({type: DELETE_DATA, payload: data} )} >delete</button>
+                    </li>
+                </div>
+            })
+          }
+       
+        </ul>
+        </div>
+   
         
-        
-        
-
+        </div>
         </div>
 
-
-
-              
-
-        
+        </div>
       );
-    }
+}
   }
   
   export default NoteApp;
